c70f1d6305f5602ef00bb01475719dfbde341c3b
Nobuki Yamada <doorknob.nobuki.1028@icloud.com>
2026-04-17 22:35:46 +0900

    Set redirect path after login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,11 @@ class ApplicationController < ActionController::Base
   # nameパラメーターを許可
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  # ログイン後のリダイレクト
+  def after_sign_in_path_for(resource)
+    root_path
+  end
+
 protected
 
   def configure_permitted_parameters
