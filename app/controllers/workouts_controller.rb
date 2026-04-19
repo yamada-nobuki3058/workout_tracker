3d6a3fcd4bdb362784800efe8c07affc37fda18b
Nobuki Yamada <doorknob.nobuki.1028@icloud.com>
2026-04-19 22:31:20 +0900

    feat: implement workout creation logic

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,7 +1,23 @@
 class WorkoutsController < ApplicationController
+  before_action :authenticate_user!
+
   def new
+    @workout = current_user.workouts.new
   end
 
   def create
+    @workout = current_user.workouts.new(workout_params)
+
+    if @workout.save
+      redirect_to root_path, notice: "ワークアウトを作成しました"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def workout_params
+    params.require(:workout).permit(:date)
   end
 end
