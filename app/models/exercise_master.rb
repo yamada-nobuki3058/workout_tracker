af269e3807918b41319313ed4ddcda17a0954d40
Nobuki Yamada <doorknob.nobuki.1028@icloud.com>
2026-05-04 22:22:10 +0900

    prevent deletion of ExerciseMaster with dependent exercises

--- app/models/exercise_master.rb
+++ app/models/exercise_master.rb
@@ -3,7 +3,8 @@
 class ExerciseMaster < ApplicationRecord
   # 外部キーの nill を許可する
   belongs_to :user, optional: true
-  has_many :exercises
+  has_many :exercises, dependent: :restrict_with_exception
+  # TODO: 将来的に論理削除に変更
 
   enum :exercise_type, { weight: 0, cardio: 1 }
 
