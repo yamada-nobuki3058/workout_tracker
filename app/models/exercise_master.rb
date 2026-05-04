31c5968bf636ddf79da7d83218887f457d5355af
Nobuki Yamada <doorknob.nobuki.1028@icloud.com>
2026-05-04 17:51:50 +0900

    add validations and enum to ExerciseMaster

--- app/models/exercise_master.rb
+++ app/models/exercise_master.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
 class ExerciseMaster < ApplicationRecord
-  belongs_to :user
+  # 外部キーの nill を許可する
+  belongs_to :user, optional: true
+
+  enum :exercise_type, { weight: 0, cardio: 1 }
+
+  validates :name, presence: true
+  validates :exercise_type, presence: true
 end
