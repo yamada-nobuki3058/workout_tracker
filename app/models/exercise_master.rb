61af9b39409b6d743a7c29a147e8112a9aa17f0c
Nobuki Yamada <doorknob.nobuki.1028@icloud.com>
2026-05-04 17:39:50 +0900

    style: apply additional rubocop fixes with -A

--- app/models/exercise_master.rb
+++ app/models/exercise_master.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class ExerciseMaster < ApplicationRecord
   belongs_to :user
 end
--- db/migrate/20260504083024_create_exercise_masters.rb
+++ db/migrate/20260504083024_create_exercise_masters.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class CreateExerciseMasters < ActiveRecord::Migration[7.1]
   def change
     create_table :exercise_masters do |t|
