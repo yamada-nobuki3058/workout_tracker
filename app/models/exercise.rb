97b85652ab88f9c8f3678f279aaebb15bb5d1114
Nobuki Yamada <doorknob.nobuki.1028@icloud.com>
2026-05-04 20:27:05 +0900

    add associations and validations to Exercise

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,4 +1,11 @@
+# frozen_string_literal: true
+
 class Exercise < ApplicationRecord
   belongs_to :workout
   belongs_to :exercise_master
+
+  has_many :weight_sets, dependent: :destroy
+  has_many :cardio_sets, dependent: :destroy
+
+  validates :position, presence: true
 end
