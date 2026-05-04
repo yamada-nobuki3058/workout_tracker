3ab636b48753c250665fb16a53b2f51c5482eafd
Nobuki Yamada <doorknob.nobuki.1028@icloud.com>
2026-05-04 22:32:58 +0900

    add validation tests for Exercise model

--- spec/models/exercise_spec.rb
+++ spec/models/exercise_spec.rb
@@ -1,5 +1,17 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Exercise, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is valid with valid attributes' do
+    expect(build(:exercise)).to be_valid
+  end
+
+  it 'is invalid without workout' do
+    expect(build(:exercise, workout: nil)).to be_invalid
+  end
+
+  it 'is invalid without exercise_master' do
+    expect(build(:exercise, exercise_master: nil)).to be_invalid
+  end
 end
