21a3bffd58b66c00f8d8b2fd9f710703a5c3460f
Nobuki Yamada <doorknob.nobuki.1028@icloud.com>
2026-05-04 18:14:40 +0900

    add ExerciseMaster model spec for validations

--- spec/models/exercise_master_spec.rb
+++ spec/models/exercise_master_spec.rb
@@ -3,5 +3,11 @@
 require 'rails_helper'
 
 RSpec.describe ExerciseMaster, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is valid with name and exercise_type' do
+    expect(build(:exercise_master)).to be_valid
+  end
+
+  it 'is invalid without name' do
+    expect(build(:exercise_master, name: nil)).to be_invalid
+  end
 end
