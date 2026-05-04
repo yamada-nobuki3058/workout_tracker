dc7e132d1afec4b352b8950247d887e0bdbdf4cb
Nobuki Yamada <doorknob.nobuki.1028@icloud.com>
2026-05-04 17:34:07 +0900

    generate ExerciseMaster model

---
+++ app/models/exercise_master.rb
@@ -0,0 +1,3 @@
+class ExerciseMaster < ApplicationRecord
+  belongs_to :user
+end
---
+++ db/migrate/20260504083024_create_exercise_masters.rb
@@ -0,0 +1,12 @@
+class CreateExerciseMasters < ActiveRecord::Migration[7.1]
+  def change
+    create_table :exercise_masters do |t|
+      t.references :user, null: false, foreign_key: true, null: true
+      t.string :name, null: false
+      t.boolean :is_unilateral, default: false, null: false
+      t.integer :exercise_type, null: false
+
+      t.timestamps
+    end
+  end
+end
