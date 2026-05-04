d4a30f4e7fad5fc12c33590cef769278f662e986
Nobuki Yamada <doorknob.nobuki.1028@icloud.com>
2026-05-04 20:16:43 +0900

    add constraints to exercises table

--- db/migrate/20260504111035_create_exercises.rb
+++ db/migrate/20260504111035_create_exercises.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 class CreateExercises < ActiveRecord::Migration[7.1]
   def change
     create_table :exercises do |t|
       t.references :workout, null: false, foreign_key: true
       t.references :exercise_master, null: false, foreign_key: true
-      t.integer :position
+      t.integer :position, null: false
 
       t.timestamps
     end
