0064f0171d7601e8eec65167a4d7331a7f662588
Nobuki Yamada <doorknob.nobuki.1028@icloud.com>
2026-05-04 22:42:31 +0900

    add association tests for Exercise model

--- spec/models/exercise_spec.rb
+++ spec/models/exercise_spec.rb
@@ -3,15 +3,27 @@
 require 'rails_helper'
 
 RSpec.describe Exercise, type: :model do
-  it 'is valid with valid attributes' do
-    expect(build(:exercise)).to be_valid
+  describe 'validation' do
+    it 'is valid with valid attributes' do
+      expect(build(:exercise)).to be_valid
+    end
+
+    it 'is invalid without workout' do
+      expect(build(:exercise, workout: nil)).to be_invalid
+    end
+
+    it 'is invalid without exercise_master' do
+      expect(build(:exercise, exercise_master: nil)).to be_invalid
+    end
   end
 
-  it 'is invalid without workout' do
-    expect(build(:exercise, workout: nil)).to be_invalid
-  end
+  describe 'association' do
+    it 'belongs to workout' do
+      expect(described_class.reflect_on_association(:workout).macro).to eq :belongs_to
+    end
 
-  it 'is invalid without exercise_master' do
-    expect(build(:exercise, exercise_master: nil)).to be_invalid
+    it 'belongs to exercise_master' do
+      expect(described_class.reflect_on_association(:exercise_master).macro).to eq :belongs_to
+    end
   end
 end
