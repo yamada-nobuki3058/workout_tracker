735d8036771aef2678b268e169383d3ffca91210
Nobuki Yamada <doorknob.nobuki.1028@icloud.com>
2026-05-04 17:38:37 +0900

    style: auto-correct rubocop offenses

--- db/migrate/20260504083024_create_exercise_masters.rb
+++ db/migrate/20260504083024_create_exercise_masters.rb
@@ -1,7 +1,7 @@
 class CreateExerciseMasters < ActiveRecord::Migration[7.1]
   def change
     create_table :exercise_masters do |t|
-      t.references :user, null: false, foreign_key: true, null: true
+      t.references :user, null: true, foreign_key: true
       t.string :name, null: false
       t.boolean :is_unilateral, default: false, null: false
       t.integer :exercise_type, null: false
