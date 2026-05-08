f09e12f40ece9c5ec6fd0e41ced561d2c67b06a0
Nobuki Yamada <doorknob.nobuki.1028@icloud.com>
2026-05-08 16:06:33 +0900

    add constraints to cardio_sets table

--- db/migrate/20260508070107_create_cardio_sets.rb
+++ db/migrate/20260508070107_create_cardio_sets.rb
@@ -1,10 +1,12 @@
+# frozen_string_literal: true
+
 class CreateCardioSets < ActiveRecord::Migration[7.1]
   def change
     create_table :cardio_sets do |t|
       t.references :exercise, null: false, foreign_key: true
-      t.decimal :distance
-      t.integer :duration
-      t.integer :position
+      t.decimal :distance, precision: 6, scale: 3, null: false
+      t.integer :duration, null: false
+      t.integer :position, null: false
 
       t.timestamps
     end
