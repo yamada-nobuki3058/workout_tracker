b7f9fdad321a1b4a5905bb292b97edc432f15852
Nobuki Yamada <doorknob.nobuki.1028@icloud.com>
2026-05-08 17:06:34 +0900

    add validation tests for BodyDataMaster model

--- spec/models/body_data_master_spec.rb
+++ spec/models/body_data_master_spec.rb
@@ -1,5 +1,23 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe BodyDataMaster, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validation' do
+    it 'is valid with valid attributes' do
+      expect(build(:body_data_master)).to be_valid
+    end
+
+    it 'is invalid without name' do
+      expect(build(:body_data_master, name: nil)).to be_invalid
+    end
+
+    it 'is invalid without unit' do
+      expect(build(:body_data_master, unit: nil)).to be_invalid
+    end
+
+    it 'is invalid without position' do
+      expect(build(:body_data_master, position: nil)).to be_invalid
+    end
+  end
 end
