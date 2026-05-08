fa22486acd3b9966c12d6f71e80f6a52664a961e
Nobuki Yamada <doorknob.nobuki.1028@icloud.com>
2026-05-08 16:15:23 +0900

    add validation tests for CardioSet model

--- spec/models/cardio_set_spec.rb
+++ spec/models/cardio_set_spec.rb
@@ -1,5 +1,23 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe CardioSet, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validation' do
+    it 'is valid with valid attributes' do
+      expect(build(:cardio_set)).to be_valid
+    end
+
+    it 'is invalid without distance' do
+      expect(build(:cardio_set, distance: nil)).to be_invalid
+    end
+
+    it 'is invalid without duration' do
+      expect(build(:cardio_set, duration: nil)).to be_invalid
+    end
+
+    it 'is invalid without position' do
+      expect(build(:cardio_set, position: nil)).to be_invalid
+    end
+  end
 end
