eeced54946605cd8f1d04dea14d01ab42a23b4ad
Nobuki Yamada <doorknob.nobuki.1028@icloud.com>
2026-05-08 15:43:03 +0900

    add validation tests for WeightSet model

--- spec/models/weight_set_spec.rb
+++ spec/models/weight_set_spec.rb
@@ -1,5 +1,23 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe WeightSet, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validation' do
+    it 'is valid with valid attributes' do
+      expect(build(:weight_set)).to be_valid
+    end
+
+    it 'is invalid without weight' do
+      expect(build(:weight_set, weight: nil)).to be_invalid
+    end
+
+    it 'is invalid without reps' do
+      expect(build(:weight_set, reps: nil)).to be_invalid
+    end
+
+    it 'is invalid without position' do
+      expect(build(:weight_set, position: nil)).to be_invalid
+    end
+  end
 end
