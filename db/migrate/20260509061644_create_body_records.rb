711468edbf142469109eb921fe29b8febee5c409
Nobuki Yamada <doorknob.nobuki.1028@icloud.com>
2026-05-09 15:20:31 +0900

    Update body_records migration

--- db/migrate/20260509061644_create_body_records.rb
+++ db/migrate/20260509061644_create_body_records.rb
@@ -4,8 +4,8 @@ class CreateBodyRecords < ActiveRecord::Migration[7.1]
   def change
     create_table :body_records do |t|
       t.references :body_data_master, null: false, foreign_key: true
-      t.date :date
-      t.decimal :value
+      t.date :date, nill: false
+      t.decimal :value, nill: false, precision: 5, scale: 2
 
       t.timestamps
     end
