00d212e17e8e2a7bf5ed6af016493a30d0eac774
Nobuki Yamada <doorknob.nobuki.1028@icloud.com>
2026-05-09 15:31:11 +0900

    Add validation specs for BodyRecord

--- spec/models/body_record_spec.rb
+++ spec/models/body_record_spec.rb
@@ -3,5 +3,20 @@
 require 'rails_helper'
 
 RSpec.describe BodyRecord, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validation' do
+    it 'is valid with valid attributes' do
+      body_record = build(:body_record)
+      expect(body_record).to be_valid
+    end
+
+    it 'is invalid without a date' do
+      body_record = build(:body_record, date: nil)
+      expect(body_record).not_to be_valid
+    end
+
+    it 'is invalid without a value' do
+      body_record = build(:body_record, value: nil)
+      expect(body_record).not_to be_valid
+    end
+  end
 end
